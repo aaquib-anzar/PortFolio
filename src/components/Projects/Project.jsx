591100dce3052de067c9a0b3f24009eed19e45f6
aaquib-anzar <aaquib.anzar17@gmail.com>
2024-06-24 13:05:02 +0530

    updated project section

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -3,6 +3,8 @@ import weather from "../../assets/Projects/weather.png";
 import countries from "../../assets/Projects/countries.png";
 import foodapp from "../../assets/Projects/FoodApp.png";
 import news from "../../assets/Projects/news.png";
+import music from "../../assets/Projects/Music_School.png";
+import cars from "../../assets/Projects/Car_rental.png";
 import Card from "./Card";
 import classes from "./Project.module.css";
 
@@ -18,21 +20,17 @@ function Project() {
         </p>
         <div className={classes.container}>
           <div className="row" style={{ paddingBottom: "10px" }}>
-            <div
-              className={`col-lg-4 mb-4 ${classes["project-card"]}`}
-            >
+            <div className={`col-lg-4 mb-4 ${classes["project-card"]}`}>
               <Card
                 imgPath={countries}
                 isBlog={false}
                 title="Where in the World?"
                 description="This project is created to know some important facts about a particular place in the world.Facts like knowing about the neighbouring countries,capital and population etc.I used ReactJs for developing the UI and countriesAPI for the data."
-                ghLink="https://github.com/aaquib-anzar/WeatherApp"
-                demoLink="https://weather-app-navy-mu-29.vercel.app/"
+                ghLink="https://github.com/aaquib-anzar/CountriesAPI"
+                demoLink="https://countries-j0pv82k9p-aaquib-anzars-projects.vercel.app/"
               />
             </div>
-            <div
-              className={` col-lg-4 mb-4 ${classes["project-card"]}`}
-            >
+            <div className={` col-lg-4 mb-4 ${classes["project-card"]}`}>
               <Card
                 imgPath={weather}
                 isBlog={false}
@@ -42,9 +40,7 @@ function Project() {
                 demoLink="https://weather-app-navy-mu-29.vercel.app/"
               />
             </div>
-            <div
-              className={`col-lg-4 mb-4 ${classes["project-card"]}`}
-            >
+            <div className={`col-lg-4 mb-4 ${classes["project-card"]}`}>
               <Card
                 imgPath={foodapp}
                 isBlog={false}
@@ -54,9 +50,7 @@ function Project() {
                 demoLink="https://lets-taste-ea1de.web.app/"
               />
             </div>
-            <div
-              className={`col-lg-4 mb-4 ${classes["project-card"]}`}
-            >
+            <div className={`col-lg-4 mb-4 ${classes["project-card"]}`}>
               <Card
                 imgPath={news}
                 isBlog={false}
@@ -66,22 +60,30 @@ function Project() {
                 demoLink="https://news-mania-5yc464tkl-aaquib-anzars-projects.vercel.app/"
               />
             </div>
-            <div
-              className={`col-lg-4 mb-4 ${classes["project-card"]}`}
-            >
-              <Card
-                imgPath={weather}
-                isBlog={false}
-                title="Weather"
-                description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-                ghLink="https://github.com/aaquib-anzar/WeatherApp"
-                demoLink="https://weather-app-navy-mu-29.vercel.app/"
-              />
-            </div>
+            <div className={`col-lg-4 mb-4 ${classes["project-card"]}`}>
+            <Card
+              imgPath={music}
+              isBlog={false}
+              title="MusicSchool"
+              description="Music School is the one stop soltuion for learning any kind of music.At music school you will get exposure to different kinds of music with our renowned faculty.I develop this project to just start my NEXTJS journey and develop the components with the help of Acertinity UI ."
+              ghLink="https://github.com/aaquib-anzar/Music_Academy"
+              demoLink="https://music-academy-fawn-eta.vercel.app/"
+            />
+          </div>
+          <div className={`col-lg-4 mb-4 ${classes["project-card"]}`}>
+            <Card
+              imgPath={cars}
+              isBlog={false}
+              title="Gaadi Lelo"
+              description="Gadi Lelo is the one stop soltuion for any kind of car.At Gaadi Lelo you can be buyer or you can be seller or you can be both.I develop this project using Python and django."
+              ghLink="https://github.com/aaquib-anzar/Car_Rental"
+              demoLink="https://github.com/aaquib-anzar/Car_Rental"
+            />
           </div>
         </div>
       </div>
-      </div>
+    </div>
+    </div>
   );
 }
 
